a09d7cd36b38de24763eb163bb8132686c5cdcd9
aryan singh <aryan@iitgn.ac.in>
2016-09-30 04:22:46 +0530

    instant search converted to debounced version using lodash

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -12,18 +13,29 @@ const API_KEY = 'AIzaSyAcfB5xzK0JTGyE6zvrqy-1847fkINsC7E';
 class App extends Component{
 	constructor(props){
 		super(props);
-		this.state = { videos: [] };
+		this.state = { videos: [],
+		 							 selectedVideo: null};
 
-		YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
-			this.setState({ videos });
+		this.videoSearch('surfboards');
+	}
+
+	videoSearch(term){
+		YTSearch({ key: API_KEY, term: term}, (videos) => {
+			this.setState({
+				videos: videos ,
+				selectedVideo: videos[0]
+			});
 		});
 	}
 	render(){
+		const videoSearch = _.debounce((term) => {this.videoSearch(term)},300);
 		return (
 			<div>
-		 		<SearchBar />
-				<VideoDetail video={this.state.videos[0]} />
-				<VideoList videos={this.state.videos} />
+		 		<SearchBar onSearchTermChange={videoSearch}/>
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					onVideoSelect={ selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos} />
 		  </div>
 		);
 	}
